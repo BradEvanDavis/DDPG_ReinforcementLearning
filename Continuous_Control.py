d8c69b0038778e414058b0851399577650b88223
Brad Davis <brad.evan.davis@gmail.com>
2019-03-11 19:27:52 -0700

    updated parameters

--- Continuous_Control.py
+++ Continuous_Control.py
@@ -1,6 +1,3 @@
-
-#get_ipython().system('pip -q install ./python')
-
 from unityagents import UnityEnvironment
 import numpy as np
 from agent import Agent
@@ -12,10 +9,10 @@ import time
 %matplotlib inline
 
 # select this option to load version 1 (with a single agent) of the environment
-env = UnityEnvironment(file_name='.\Reacher1\Reacher.exe')
+#env = UnityEnvironment(file_name='.\Reacher1\Reacher.exe')
 
 # select this option to load version 2 (with 20 agents) of the environment
-# env = UnityEnvironment(file_name='.\Reacher20\Reacher.exe')
+env = UnityEnvironment(file_name='.\Reacher20\Reacher.exe')
 
 # get the default brain
 brain_name = env.brain_names[0]
@@ -38,14 +35,7 @@ state_size = states.shape[1]
 print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
 print('The state for the first agent looks like:', states[0])
 
-mean_scores = []
-min_scores = []
-max_scores = []
-best_score = -np.inf
-scores_window = deque(maxlen=100)
-moving_avgs = []
-
-def ddpg(n_episodes=300, max_t=700, train=True, num_agents=1, print_every=20, train_mode=True):
+def ddpg(n_episodes=300, max_t=1000, train=True, num_agents=20, print_every=1, train_mode=True):
     
     mean_scores = []
     min_scores = []
@@ -53,12 +43,12 @@ def ddpg(n_episodes=300, max_t=700, train=True, num_agents=1, print_every=20, tr
     best_score = -np.inf
     scores_window = deque(maxlen=100)
     moving_avgs = []
-
+    duration = 0
     
     #for i in range(num_agents):
      #   agents.append(Agent(state_size, action_size, random_seed=44))
     
-    for i_episode in range(0, n_episodes):
+    for i_episode in range(1, n_episodes+1):
         env_info = env.reset(train_mode=True)[brain_name]
         states = env_info.vector_observations
         scores = np.zeros(num_agents)
@@ -75,12 +65,12 @@ def ddpg(n_episodes=300, max_t=700, train=True, num_agents=1, print_every=20, tr
             for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
                 agent.step(state, action, rewards, next_state, done, t)
                 
-            states = next_state
+            states = next_states
             scores += rewards
             if np.any(dones):
                 break
 
-        duration = time.time() - start_time
+        duration += time.time() - start_time
         min_scores.append(np.min(scores))
         max_scores.append(np.max(scores))
         mean_scores.append(np.mean(scores))
@@ -89,7 +79,7 @@ def ddpg(n_episodes=300, max_t=700, train=True, num_agents=1, print_every=20, tr
                 
         if i_episode % print_every == 0:
             print('\rEpisode {}, Mean last 100: {:.2f}, Mean current: {:.2f}, Max: {:.2f}, Min: {:.2f}, Time: {:.2f}'\
-                .format(i_episode, moving_avgs[-1], mean_scores[-1], max_scores[-1], min_scores[-1], duration, end="\n"))
+                .format(i_episode, moving_avgs[-1], mean_scores[-1], max_scores[-1], min_scores[-1], round(duration/60,2), end="\n"))
             torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
             torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
 
@@ -99,7 +89,7 @@ def ddpg(n_episodes=300, max_t=700, train=True, num_agents=1, print_every=20, tr
             torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
             torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
             
-    return scores
+    return mean_scores, moving_avgs, max_scores, min_scores
 
 
 
